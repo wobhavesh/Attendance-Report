ecc90f785818302e716e8272d8082401d355c0e6
bhavesh z <wo.bhavesh@gmail.com>
2023-08-25 13:15:01 +0530

    - Test webhook commit.

--- src/main/java/com/wo/AttendanceReportApplication.java
+++ src/main/java/com/wo/AttendanceReportApplication.java
@@ -12,7 +12,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import com.wo.service.file.FilesStorageService;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class AttendanceReportApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
 	@Resource
